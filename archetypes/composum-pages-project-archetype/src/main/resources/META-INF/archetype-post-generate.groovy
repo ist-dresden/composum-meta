d592e19a481d9417fe5c4a42077b9c4b5295313d
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-08-13 18:46:35 +0200

    rework artefact directory structure

---
+++ archetypes/composum-pages-project-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
@@ -0,0 +1,32 @@
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+Properties properties = request.properties
+String thetenant = properties.getProperty("tenant")
+String thedomain = properties.getProperty("domain")
+String theproject = properties.getProperty("project")
+
+Map<String, String> dirnamemap = ["tenant": thetenant, "domain": thedomain, "project": theproject]
+// println("dirnamemap: " + dirnamemap)
+
+Path projectPath = Paths.get(request.outputDirectory, request.artifactId)
+// println("projectPath: " + projectPath)
+
+void renameDirectories(Path path, Map<String, String> dirnamemap) {
+    for (Path child : Files.newDirectoryStream(path)) {
+        if (Files.isDirectory(child)) {
+            String childName = child.getFileName().toString()
+            if (dirnamemap.containsKey(childName)) {
+                String newname = dirnamemap.get(childName)
+                Path renamed = Files.move(child, path.resolve(newname))
+                println("Renaming " + child.toString() + " to " + renamed.toString())
+                renameDirectories(renamed, dirnamemap)
+            } else {
+                renameDirectories(child, dirnamemap)
+            }
+        }
+    }
+}
+
+renameDirectories(projectPath, dirnamemap)
